6e5f1730c09ad13cfb852fd36850291bc06d9c37
kpamukcu <kapawebtasarim@gmail.com>
2024-08-17 13:06:26 +0300

    sql update

--- mecmot.sql
+++ mecmot.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Anamakine: localhost
--- Üretim Zamanı: 14 Ağu 2024, 20:43:55
+-- Üretim Zamanı: 17 Ağu 2024, 12:05:12
 -- Sunucu sürümü: 10.4.28-MariaDB
 -- PHP Sürümü: 8.2.4
 
@@ -41,6 +41,29 @@ INSERT INTO `admin` (`id`, `kadi`, `sifre`) VALUES
 (1, 'Admin', '123'),
 (2, 'Kaan', '456');
 
+-- --------------------------------------------------------
+
+--
+-- Tablo için tablo yapısı `kategoriler`
+--
+
+CREATE TABLE `kategoriler` (
+  `id` int(11) NOT NULL,
+  `katAdi` varchar(50) NOT NULL,
+  `katTuru` varchar(50) NOT NULL,
+  `ustKat` varchar(100) NOT NULL,
+  `aciklama` text NOT NULL,
+  `gorsel` varchar(100) NOT NULL,
+  `katDili` varchar(25) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Tablo döküm verisi `kategoriler`
+--
+
+INSERT INTO `kategoriler` (`id`, `katAdi`, `katTuru`, `ustKat`, `aciklama`, `gorsel`, `katDili`) VALUES
+(1, 'Vidalı Krikolar', 'Üst Kategori', '-', 'Vidalı Kriko Meta Description', '../assets/img/vidali-krikolar-48x48.webp', 'Türkçe');
+
 --
 -- Dökümü yapılmış tablolar için indeksler
 --
@@ -52,6 +75,12 @@ ALTER TABLE `admin`
   ADD PRIMARY KEY (`id`);
 
 --
+-- Tablo için indeksler `kategoriler`
+--
+ALTER TABLE `kategoriler`
+  ADD PRIMARY KEY (`id`);
+
+--
 -- Dökümü yapılmış tablolar için AUTO_INCREMENT değeri
 --
 
@@ -60,6 +89,12 @@ ALTER TABLE `admin`
 --
 ALTER TABLE `admin`
   MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+
+--
+-- Tablo için AUTO_INCREMENT değeri `kategoriler`
+--
+ALTER TABLE `kategoriler`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
