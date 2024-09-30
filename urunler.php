8610a4db1a1a70ccf38e8edebb1347fe723ab022
kpamukcu <kapawebtasarim@gmail.com>
2024-09-30 14:31:56 +0300

    frontend urunler page create

---
+++ urunler.php
@@ -0,0 +1,30 @@
+<?php require_once('header.php'); ?>
+
+<!-- Product Groups Section Start -->
+<section id="products">
+    <div class="container">
+        <div class="row">
+            <a href="">
+                <div class="box">
+                    <img src="./assets/img/vidali-krikolar-48x48.webp" alt="Vidalı Krikolar">
+                    <span>Vidalı Krikolar</span>
+                </div>
+            </a>
+            <a href="">
+                <div class="box">
+                    <img src="./assets/img/vidali-krikolar-48x48.webp" alt="Vidalı Krikolar">
+                    <span>Vidalı Krikolar</span>
+                </div>
+            </a>
+            <a href="">
+                <div class="box">
+                    <img src="./assets/img/vidali-krikolar-48x48.webp" alt="Vidalı Krikolar">
+                    <span>Vidalı Krikolar</span>
+                </div>
+            </a>
+        </div>
+    </div>
+</section>
+<!-- Product Groups Section End -->
+
+<?php require_once('footer.php'); ?>
